7e1af8be94023b14563e881241f1589071de80cf
alanzhang03 <107202923+alanzhang03@users.noreply.github.com>
2025-02-24 13:29:18 -0500

    updated host link

--- ids-frontend/src/utils/api.js
+++ ids-frontend/src/utils/api.js
@@ -1,44 +1,44 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/threats";
+const API_URL = "http://localhost:8080/api/threats"; 
 
 export const fetchThreats = async () => {
-	try {
-		const response = await axios.get(API_URL);
-		return response.data;
-	} catch (error) {
-		console.error("Error fetching threats:", error);
-		return [];
-	}
+  try {
+    const response = await axios.get(API_URL);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching threats:", error);
+    return [];
+  }
 };
 
 export const addThreat = async (threat) => {
-	try {
-		const response = await axios.post(API_URL, threat, {
-			headers: { "Content-Type": "application/json" },
-		});
-		return response.data;
-	} catch (error) {
-		console.error("Error adding threat:", error);
-	}
+  try {
+    const response = await axios.post(API_URL, threat, {
+      headers: { "Content-Type": "application/json" },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error adding threat:", error);
+  }
 };
 
 export const updateThreat = async (id, updatedThreat) => {
-	try {
-		const response = await axios.put(`${API_URL}/${id}`, updatedThreat, {
-			headers: { "Content-Type": "application/json" },
-		});
-		return response.data;
-	} catch (error) {
-		console.error("Error updating threat:", error);
-		throw new Error("Failed to update threat. Please try again.");
-	}
+  try {
+    const response = await axios.put(`${API_URL}/${id}`, updatedThreat, {
+      headers: { "Content-Type": "application/json" },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error updating threat:", error);
+    throw new Error("Failed to update threat. Please try again.");
+  }
 };
 
 export const deleteThreat = async (id) => {
-	try {
-		await axios.delete(`${API_URL}/${id}`);
-	} catch (error) {
-		console.error("Error deleting threat:", error);
-	}
+  try {
+    await axios.delete(`${API_URL}/${id}`);
+  } catch (error) {
+    console.error("Error deleting threat:", error);
+  }
 };
