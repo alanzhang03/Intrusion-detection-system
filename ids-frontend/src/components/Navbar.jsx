c5ea05740adc880032e254501e28f6e32d2b281c
alanzhang03 <107202923+alanzhang03@users.noreply.github.com>
2025-02-27 14:16:19 -0500

    navbar

--- ids-frontend/src/components/Navbar.jsx
+++ ids-frontend/src/components/Navbar.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import styles from "../styles/Navbar.module.scss"
-
+import React from "react";
+import styles from "../styles/Navbar.module.scss";
 
 const Navbar = () => {
   return (
-    <>
-      <div className={styles.body}>
-
-
+    <nav className={styles.navbar}>
+      <div className={styles.navCont}>
+        <div className={styles.logo}>IDS Logo</div>
+        <ul className={styles.navLinks}>
+          <li>Home</li>
+          <li>About</li>
+          <li>Testing</li>
+        </ul>
       </div>
-    </>
-  )
-}
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
