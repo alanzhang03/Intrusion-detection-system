f1c26817685cb284da70c88f25890cbb37f3b6d8
Alan Zhang <alan.s.zhang@gmail.com>
2025-03-02 23:32:18 -0500

    footer

--- ids-frontend/src/components/Footer.jsx
+++ ids-frontend/src/components/Footer.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import styles from "../styles/Footer.module.scss"
-
+import React from "react";
+import styles from "../styles/Footer.module.scss";
 
 const Footer = () => {
-  return (
-    <>
-      <div className={styles.body}></div>
-    
-    </>
-  )
-}
+	return (
+		<footer className={styles.footer}>
+			<div className={styles.footerContent}>
+				<p>&copy; {new Date().getFullYear()}</p>
+				<ul className={styles.footerLinks}>
+					<li>
+						<a href="#">Privacy Policy</a>
+					</li>
+					<li>
+						<a href="#">Terms of Service</a>
+					</li>
+					<li>
+						<a href="#">Contact</a>
+					</li>
+				</ul>
+			</div>
+		</footer>
+	);
+};
 
-export default Footer
+export default Footer;
